779a342616216ce15240a29d3e4648185e5614a3
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-05 16:58:53 +0200

    ....

---
+++ src/app/heroes/pages/get/get.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get',
+  templateUrl: './get.component.html',
+  styles: [
+  ]
+})
+export class GetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
