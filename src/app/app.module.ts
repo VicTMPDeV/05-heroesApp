07d1c21454011640862eedd699b1ab8e7af0ecb8
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-08 18:45:12 +0200

    add heroes service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+//Angular Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from "@angular/common/http";
+//My Modules
 import { AppRoutingModule } from './app-routing.module';
-
+//My Components
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
@@ -13,8 +15,10 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
     ErrorPageComponent //Como la carpeta shared no tiene un módulo, busca un módulo en el que declararlo en las carpetas padre, por eso lo declara aqui
   ],
   imports: [
+    //TODO -> Preguntar a Efi
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule, //Aquí porque los servicios van a ser proveídos de manera global -> Podríamos haber usado el fetch API de JS, pero con Observables tenemos más control
     AppRoutingModule
   ],
   providers: [],
