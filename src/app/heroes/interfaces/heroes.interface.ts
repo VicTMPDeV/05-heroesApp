ae2de9191f8350c42258916285962e335b952ba5
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-08 18:47:45 +0200

    add heroes interface for avoid Object type

---
+++ src/app/heroes/interfaces/heroes.interface.ts
@@ -0,0 +1,14 @@
+export interface Heroe {
+    id?:              string; //Cuando lo traigo del servicio viene, pero al crear uno nuevo, de momento no lo tenemos
+    superhero:        string;
+    publisher:        Publisher;
+    alter_ego:        string;
+    first_appearance: string;
+    characters:       string;
+    alt_img?:         string; //Puede ser que no tengamos la imagen cuando creemos un heroe nuevo, y será una url
+}
+
+export enum Publisher {
+    DCComics = "DC Comics",
+    MarvelComics = "Marvel Comics",
+}
