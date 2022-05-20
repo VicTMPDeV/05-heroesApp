7d7b88478316716d211c73520be4778a5a8fd8a9
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-20 20:54:24 +0200

    heroes form phase 1

--- src/app/heroes/pages/addHero/addHero.component.ts
+++ src/app/heroes/pages/addHero/addHero.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-add',
   templateUrl: './addHero.component.html',
-  styles: [
-  ]
+  styles: [`
+    button{
+      margin-top:1.5rem;
+    }
+  `]
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public hero: Hero = {
+    superhero:'',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+    publisher: Publisher.DCComics,
+    alt_img: ''
   }
+
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  save(){
+    console.log(this.hero);
+  }
+
 
 }
